ff57a088f41bbf223f1ddd41095f6161c2ffc42f
bimanathanael <bimanathanael95@gmail.com>
2020-07-26 19:34:09 +0700

    Update calcMaxProfit.js
    fix test case from array to string

--- calcMaxProfit.js
+++ calcMaxProfit.js
@@ -1,15 +1,15 @@
 
-
 //test case
-const test1 = [5,4,3,2,1];
-const test2 = [3,2,8,4,8,6,0,7,9];
-const test3 = [3,2,1,5,6,2];
-const test4 = [1,3,2,5,1,6,9,0,1]; 
-const test5 = [3,3,5,0,0,9,2,7,7];
-const test6 = [1,4,5,9,1,1,6,4,4];
+const test1 = '54321';
+const test2 = '328486079';
+const test3 = '321562';
+const test4 = '132516901';
+const test5 = '335009277';
+const test6 = '145911644';
 
 //begin code
-function calcMaxProfit(n) {
+function calcMaxProfit(arrs) {
+  let n = arrs.split('')
   let maxProfit = 0 
   for(let i = 0 ; i < n.length-1 ; i++) {
     const maxNum = Math.max(...n.slice(i+1));
